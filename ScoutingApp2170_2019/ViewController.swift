5ee599e1626bf808d03bc2a0d0b61896ecabaa4b
Leonard Liu <leonardliu@leonards-macbook-pro.local>
2019-01-15 17:27:13 -0500

    Initial Commit

---
+++ ScoutingApp2170_2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScoutingApp2170_2019
+//
+//  Created by Leonard Liu on 1/15/19.
+//  Copyright © 2019 Leonard Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
